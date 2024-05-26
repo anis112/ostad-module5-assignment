eed6f4bc226fede80958b8c6fdfdd414d8f10f5c
Anisur Rahman <anis112@yahoo.com>
2024-05-27 02:50:30 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -31,11 +31,37 @@ function validate() {
         validation_message = "Message can not be empty!";
         is_valid = false;
     }
-    
+
     if(is_valid) {
         alert("You have submitted your contact information successfully!");
     }
     else {
         alert(validation_message);
     }
+}
+
+function confirm_basic_package_selection () {
+    document.getElementById("info-message-basic-package").innerHTML="Thank you for choosing 1 basic package";
+    document.getElementById("info-message-pro-package").innerHTML="";
+}
+
+function confirm_pro_package_selection() {
+    document.getElementById("info-message-basic-package").innerHTML="";
+    document.getElementById("info-message-pro-package").innerHTML="Thank you for choosing 1 pro package";
+}
+
+function basic_minus_button() {    
+    document.getElementById("basic-room-info").innerHTML = "";   
+}
+
+function basic_plus_button() {    
+    document.getElementById("basic-room-info").innerHTML = "1 room";    
+}
+
+function pro_minus_button() {    
+    document.getElementById("pro-room-info").innerHTML = "";   
+}
+
+function pro_plus_button() {    
+    document.getElementById("pro-room-info").innerHTML = "1 room";    
 }
